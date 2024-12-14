191c5a39d89dd1b53f9ec690ab1fb18088076fae
KirillDanProg <kirill2898v@gmail.com>
2024-12-14 14:34:00 +0300

    feat: remove redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/ui/app";
-import StoreProvider from "./app/providers/store.provider/ui/store.provider";
 
 const root = createRoot(document.getElementById("root") as HTMLDivElement);
 
-root.render(
-  <StoreProvider>
-    <App />
-  </StoreProvider>
-);
+root.render(<App />);
