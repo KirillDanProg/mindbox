5b156558bd7d8bdf9e579bf42b421ca6446a856a
KirillDanProg <kirill2898v@gmail.com>
2024-12-14 19:21:51 +0300

    feat: add css styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/ui/app";
+import "./app/style/index.css";
 
 const root = createRoot(document.getElementById("root") as HTMLDivElement);
 
