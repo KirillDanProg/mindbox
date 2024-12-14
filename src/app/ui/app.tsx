61370e8af424175414cce006ff3c357a2f6e3802
KirillDanProg <kirill2898v@gmail.com>
2024-12-14 19:22:48 +0300

    feat: add todos page

--- src/app/ui/app.tsx
+++ src/app/ui/app.tsx
@@ -1,4 +1,11 @@
+import { TodosPage } from "@/pages/todos";
+
 const App = () => {
-  return <div>test</div>;
+  return (
+    <div className="flex flex-col align-center min-h-screen p-4 text-center bg-gray-200 ">
+      <h1 className="font-thin text-8xl text-red-900 opacity-20">todos</h1>
+      <TodosPage />
+    </div>
+  );
 };
 export default App;
---
+++ src/pages/todos/ui/todos.page.tsx
@@ -0,0 +1,11 @@
+import { TodoList } from "@/widgets/todo.list";
+
+const TodosPage = () => {
+  return (
+    <div className="flex align-center justify-center">
+      <TodoList />
+    </div>
+  );
+};
+
+export default TodosPage;
