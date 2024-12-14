fe50201a1e361b24b505e8429360e886607bd7b9
KirillDanProg <kirill2898v@gmail.com>
2024-12-14 19:26:43 +0300

    feat: add styles for active filter tab

--- src/entities/todos.filter.tab/index.ts
+++ src/entities/todos.filter.tab/index.ts
@@ -1,1 +1,2 @@
 export { FILTER_TABS, type FilterTabsType } from "./model/types/filter.values";
+export { default as TodosFilterTab } from "./ui/todos.filter.tab";
--- src/entities/todos.filter.tab/ui/todos.filter.tab.tsx
+++ src/entities/todos.filter.tab/ui/todos.filter.tab.tsx
@@ -2,17 +2,27 @@ import { FILTER_TABS, type FilterTabsType } from "../model/types/filter.values";
 
 type TodosFilterTabProps = {
   tabKey: FilterTabsType;
+  activeTab: FilterTabsType;
   callback: (value: FilterTabsType) => void;
 };
 
 const TodosFilterTab = (props: TodosFilterTabProps) => {
-  const { tabKey, callback } = props;
+  const { tabKey, activeTab, callback } = props;
 
   const onClickHandler = () => {
     callback(tabKey);
   };
 
-  return <div onClick={onClickHandler}>{`${FILTER_TABS[tabKey]}`}</div>;
+  const isActive = tabKey === activeTab;
+
+  return (
+    <div
+      className={`cursor-pointer px-2 rounded-sm border ${
+        isActive ? "border-red-100" : "border-transparent"
+      } `}
+      onClick={onClickHandler}
+    >{`${FILTER_TABS[tabKey]}`}</div>
+  );
 };
 
 export default TodosFilterTab;
