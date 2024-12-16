c2fa6c88194a3e2ce364038a0a18116da520d73b
KirillDanProg <kirill2898v@gmail.com>
2024-12-16 20:43:27 +0300

    feat: add notistack for snack bar notifications

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/ui/app";
 import "./app/style/index.css";
+import { SnackbarProvider } from "notistack";
 
 const root = createRoot(document.getElementById("root") as HTMLDivElement);
 
-root.render(<App />);
+root.render(
+  <SnackbarProvider>
+    <App />
+  </SnackbarProvider>
+);
